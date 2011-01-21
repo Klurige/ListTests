3f3930e16dbe96175d494e26e7278fc95e4ff379
Robert Niemi <robert.niemi@enea.com>
2011-01-21 21:14:37 +0100

    Got the preconditions test to work.

--- src/cc/co/klurige/list/test/DatabaseTests.java
+++ src/cc/co/klurige/list/test/DatabaseTests.java
@@ -1,32 +1,35 @@
 package cc.co.klurige.list.test;
 
+import android.content.Context;
 import android.test.AndroidTestCase;
-import android.test.mock.MockContext;
+import android.test.IsolatedContext;
 import cc.co.klurige.list.database.DatabaseAdapter;
 
 public class DatabaseTests extends AndroidTestCase {
 
   private DatabaseAdapter mDbHelper;
-  private MockContext     mCtx;
+  private Context         mCtx;
 
   @Override
   protected void setUp() throws Exception {
     super.setUp();
-    mCtx = new MockContext();
+    mCtx = new IsolatedContext(null, this.getContext());
     setContext(mCtx);
-    // mDbHelper = DatabaseAdapter.getDatabaseAdapter(mCtx);
+    mDbHelper = DatabaseAdapter.getDatabaseAdapter(mCtx);
 
-    // mDbHelper.open();
-
+    mDbHelper.open();
   }
 
   @Override
   protected void tearDown() throws Exception {
     super.tearDown();
-    // mDbHelper.delete();
+    mDbHelper.close();
+    mDbHelper.delete();
   }
 
   public void testPreConditions() {
-    assertFalse(true);
+    assertNotNull(mDbHelper);
+    assertTrue(mDbHelper.getDB().isOpen());
   }
+
 }
