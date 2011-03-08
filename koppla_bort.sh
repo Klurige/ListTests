7c05d969da48da3c8d5771a05b5dadfa3adf4bda
Robert Niemi <robert.den.klurige@gmail.com>
2011-03-08 21:59:26 +0100

    Added the missing tests

--- koppla_bort.sh
+++ koppla_bort.sh
@@ -2,6 +2,9 @@
 mkdir -p bortkopplade/src/cc/co/klurige/list/
 mv src/cc/co/klurige/list/database bortkopplade/src/cc/co/klurige/list/.
 mv src/cc/co/klurige/list/CategoriesActivityTests.java bortkopplade/src/cc/co/klurige/list/.
+mv src/cc/co/klurige/list/UnitsActivityTests.java bortkopplade/src/cc/co/klurige/list/.
+mv src/cc/co/klurige/list/ListsActivityTests.java bortkopplade/src/cc/co/klurige/list/.
+mv src/cc/co/klurige/list/ItemEditActivityTests.java bortkopplade/src/cc/co/klurige/list/.
 
 rm -rf bin gen/* coverage instrumented libs
 adb uninstall cc.co.klurige.list
